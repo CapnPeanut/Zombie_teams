c62e5578fdfe11ef68aff5e83887f01f0695acd1
Brian Atchley <CaptainPeanut@Brians-MacBook-Pro.local>
2015-03-04 19:02:23 -0500

    Initial Commit

---
+++ z teams 3Tests/z_teams_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  z_teams_3Tests.swift
+//  z teams 3Tests
+//
+//  Created by Brian Atchley on 3/4/15.
+//  Copyright (c) 2015 Brian Atchley. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class z_teams_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
